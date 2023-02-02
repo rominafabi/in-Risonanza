77474155f93408279ec54343ab36679924ab4aac
rominafabi <rominafabibusiness@gmail.com>
2023-02-02 07:34:52 +0100

    Feat: Imported Material-Tailwind and Custom Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
+    fontFamily: {
+      openSans: ["openSans", "sans-serif"],
+      semiBold: ["semiBold", "sans-serif"],
+      openSansBold: ["openSansBold", "sans-serif"],
+      uniNeueLight: ["uniNeueLight", "sans-serif"],
+      uniNeueBook: ["uniNeueBook", "sans-serif"],
+      uniNeueRegular: ["uniNeueRegular", "sans-serif"],
+      uniNeueBold: ["uniNeueBold", "sans-serif"],
+    },
+    colors: {
+      main: "#02C3A5",
+      secondary: "#d8ebe8",
+      hearth: "#ED5C5C",
+      paragraph: "#1a2734",
+    },
     extend: {},
   },
   plugins: [],
-};
+});
